cfd88d622b55cee8dd3dbbb128e5647e136e6d60
rsmith <rsmith5901@gmail.com>
2022-01-28 11:37:02 -0500

    asdf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(monitor.mydevicespot.com, monitor.mydevicespot.com)
+domains=(monitor.mydevicespot.com monitor.mydevicespot.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
