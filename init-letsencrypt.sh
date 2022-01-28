ca15ce45f4ff262693da20db1359411986738f6e
rsmith <rsmith5901@gmail.com>
2022-01-28 12:21:56 -0500

    asdf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -57,24 +57,25 @@ for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# # Select appropriate email arg
-# case "$email" in
-#   "") email_arg="--register-unsafely-without-email" ;;
-#   *) email_arg="--email $email" ;;
-# esac
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
 
-# # Enable staging mode if needed
-# if [ $staging != "0" ]; then staging_arg="--staging"; fi
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-# docker-compose run --rm --entrypoint "\
-#   certbot certonly --webroot -w /var/www/certbot \
-#     $staging_arg \
-#     $email_arg \
-#     $domain_args \
-#     --rsa-key-size $rsa_key_size \
-#     --agree-tos \
-#     --force-renewal" certbot
-# echo
+docker-compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
 
-# echo "### Reloading nginx ..."
-# docker-compose exec nginx nginx -s reload
+echo "### Reloading nginx ..."
+docker-compose exec nginx nginx -s reload
+docker-compose exec grafana grafana -s reload
